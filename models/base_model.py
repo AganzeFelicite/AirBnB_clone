a94c2ae64c7543dd8fed45d06999a42617b29ae6
AGANZE FELICITE <99313069+AganzeFelicite@users.noreply.github.com>
2022-08-08 09:10:42 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,17 @@ class BaseModel:
         """
         initialisation of instance attributes
         """
-        for key, val in kwargs.items():
-            if key == "__class__":
-                continue
-            if val in ("created_at", "updated_at"):
-                val = datetime.fromisoformat(val)
-                setattr(self, key, val)
-        
-        self.id = str(uuid.uuid4())  # to assign a unique id
-        self.created_at = datetime.now()  # to store the time
-        self.updated_at = datetime.now()
+        if kwargs != {}:
+            for key, val in kwargs.items():
+                if key == "__class__":
+                    continue
+                if val in ("created_at", "updated_at"):
+                    val = datetime.fromisoformat(val)
+            setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())  # to assign a unique id
+            self.created_at = datetime.now()  # to store the time
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''this returns [class name] (id) <all the methods of the class'''
